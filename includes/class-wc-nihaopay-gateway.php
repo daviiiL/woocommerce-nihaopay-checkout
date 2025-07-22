3f3debc73e11f00f6dac321824888bc7fcf51cb9
David Liu <davidas99017@gmail.com>
2025-07-21 19:39:33 -0700

    added classic checkout support

--- includes/class-wc-nihaopay-gateway.php
+++ includes/class-wc-nihaopay-gateway.php
@@ -29,7 +29,6 @@ class WC_Nihaopay_Gateway extends WC_Payment_Gateway
 
   public function __construct()
   {
-    $this->icon = apply_filters("woocommerce_nihaopay_gateway_icon", "");
     // has_fields shows payment fields on checkout
     $this->has_fields = true;
     $this->method_title = "NihaoPay Checkout";
@@ -42,14 +41,18 @@ class WC_Nihaopay_Gateway extends WC_Payment_Gateway
     $this->currency = $this->get_option("currency");
     $this->mode = $this->get_option("mode");
     $this->enable_alipay = $this->get_option("enable_alipay");
-    $this->enable_wechatpay = $this->get_option("enable_wechat");
+    $this->enable_wechatpay = $this->get_option("enable_wechatpay");
     $this->enable_unionpay = $this->get_option("enable_unionpay");
+
+    $plugin_dir = WC_Nihaopay_Checkout::plugin_url();
+    $this->wechatpay_icon = apply_filters('woocommerce_nihaopay_wechatpay_icon', '' . $plugin_dir . '/resources/images/wechatpay_logo.png');
+    $this->alipay_icon = apply_filters('woocommerce_nihaopay_alipay_icon', '' . $plugin_dir . '/resources/images/alipay_logo.png');
+    $this->unionpay_icon = apply_filters('woocommerce_nihaopay_unionpay_icon', '' . $plugin_dir . '/resources/images/unionpay_logo.png');
 
     add_action("woocommerce_update_options_payment_gateways_" . $this->id, [
       $this,
       "process_admin_options",
     ]);
-    //TODO: add block support
   }
 
   public function admin_options()
@@ -57,27 +60,100 @@ class WC_Nihaopay_Gateway extends WC_Payment_Gateway
 ?>
     <h2><?php esc_html_e(
           "WooCommerce NihaoPay Checkout",
-          "woocommerce-nihaopay-gateway",
+          "woocommerce-nihaopay-checkout",
         ); ?></h2>
     <table class="form-table">
       <?php $this->generate_settings_html(); ?>
     </table>
+  <?php
+  }
+
+
+  function get_icon()
+  {
+    $icon = '';
+
+    if ($this->wechatpay_icon) {
+      $icon .= '<img src="' . $this->force_ssl($this->wechatpay_icon) . '" alt="' . $this->title . '" width="29" height="26" style="user-select: none;" />';
+    }
+
+    if ($this->alipay_icon) {
+      $icon .= '<img src="' . $this->force_ssl($this->alipay_icon) . '" alt="' . $this->title . '" width="26" height="26" style="user-select: none;" />';
+    }
+
+    if ($this->unionpay_icon) {
+      $icon .= '<img src="' . $this->force_ssl($this->unionpay_icon) . '" alt="' . $this->title . '" width="42" height="26" style="user-select: none;" />';
+    }
+
+    if ($icon) {
+      $icon = '<div class="nihaopay-icons" style="display: inline-flex; gap: 8px; align-items: center; width:min-content; ">' . $icon . '</div>';
+    }
+
+    return apply_filters('woocommerce_gateway_icons', $icon, $this->id);
+  }
+
+  function payment_fields()
+  {
+    $isWechatPayEnabled =
+      null !== $this->get_option("enable_wechatpay") &&
+      $this->get_option("enable_wechatpay") === "yes";
+
+    $isAliPayEnabled =
+      null !== $this->get_option("enable_alipay") &&
+      $this->get_option("enable_alipay") === "yes";
+
+    $isUnionPayEnabled =
+      null !== $this->get_option("enable_unionpay") &&
+      $this->get_option("enable_unionpay") === "yes";
+
+    global $woocommerce;
+  ?>
+    <fieldset>
+      <legend class="payment-method-title"><label>Method of payment<span class="required">*</span></label></legend>
+      <ul class="wc_payment_methods payment_methods methods">
+        <?php if ($isAliPayEnabled) : ?>
+          <li class="wc_payment_method">
+            <input id="nihaopay_pay_method_alipay"
+              class="input-radio"
+              name="vendor"
+              checked="checked"
+              value="alipay"
+              data-order_button_text="" type="radio" required>
+            <label for="nihaopay_pay_method_alipay">AliPay</label>
+          </li>
+        <?php endif; ?>
+        <?php if ($isWechatPayEnabled) : ?>
+          <li class="wc_payment_method">
+            <input id="nihaopay_pay_method_wechatpay"
+              class="input-radio"
+              name="vendor"
+              value="wechatpay"
+              data-order_button_text=""
+              type="radio"
+              required
+              <?php echo !$isAliPayEnabled ?  'checked=\"checked\"' : ""; ?>>
+            <label for="nihaopay_pay_method_wechatpay">WeChatPay</label>
+          </li>
+        <?php endif; ?>
+        <?php if ($isUnionPayEnabled) : ?>
+          <li class="wc_payment_method">
+            <input id="nihaopay_pay_method_unionpay"
+              class="input-radio"
+              name="vendor"
+              value="unionpay"
+              data-order_button_text=""
+              type="radio"
+              required
+              <?php echo !($isWechatPayEnabled && $isAliPayEnabled) ? 'checked=\"checked\"' : ""; ?>>
+            <label for="nihaopay_pay_method_unionpay">UnionPay</label>
+          </li>
+        <?php endif; ?>
+      </ul>
+      <div class="clear"></div>
+    </fieldset>
 <?php
   }
 
-  /* public function payment_fields() */
-  /* { */
-  /*     ?> */
-  /*     <fieldset> */
-  /*            <legend class="payment-method-title"> */
-  /*                <label><?php esc_html_e( */
-  /*                    "Method of payment", */
-  /*                    "woocommerce-nihaopay-checkout", */
-  /*                ); ?><span class="required">*</span></label> */
-  /*            </legend> */
-  /*     </fieldset> */
-  /*     <?php */
-  /* } */
 
   public function init_form_fields()
   {
@@ -188,5 +264,13 @@ class WC_Nihaopay_Gateway extends WC_Payment_Gateway
         "desc_tip" => true,
       ],
     ];
+  }
+
+  private function force_ssl($url)
+  {
+    if ('yes' == get_option('woocommerce_force_ssl_checkout')) {
+      $url = str_replace('http:', 'https:', $url);
+    }
+    return $url;
   }
 }
